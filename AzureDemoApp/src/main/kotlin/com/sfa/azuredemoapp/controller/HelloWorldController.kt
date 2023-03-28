79c317e96a0209721455c0ae6b64dbe04dc1b554
sfa <simone.falzone@aeb.com>
2023-03-28 17:26:03 +0200

    Test pipeline trigger

--- AzureDemoApp/src/main/kotlin/com/sfa/azuredemoapp/controller/HelloWorldController.kt
+++ AzureDemoApp/src/main/kotlin/com/sfa/azuredemoapp/controller/HelloWorldController.kt
@@ -11,6 +11,6 @@ class HelloWorldController {
 
     @GetMapping("/hello")
     fun hello(): String {
-        return "Hello from Azure!!!"
+        return "Hello from Azure!!! New Version!!!"
     }
 }
